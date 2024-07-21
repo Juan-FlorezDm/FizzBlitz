977720f9d76a985931efdd954aa155d45754b048
Juan-FlorezDm <jnendez38@gmail.com>
2024-07-20 23:17:32 -0500

    despues de cada consulta se limpiar el content

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 const btn = document.getElementById("Enviar")
+const resp = document.getElementById("respuesta")
 
 btn.addEventListener("click",()=>{
     
     const Ingresar = document.getElementById("Ingresar").value
     const cont =  document.getElementById("respuesta")
-
+    cont.innerHTML = ""
     for(var i = 1; i <= Ingresar; i++){
         const res = document.createElement("p")
         if(i%3===0 && i%5===0){
@@ -22,7 +23,7 @@ btn.addEventListener("click",()=>{
         }else{
             res.textContent = i
             cont.appendChild(res)
-            
         }
     }
 })
+
