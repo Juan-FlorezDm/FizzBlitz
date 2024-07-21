8f79d1104c5f9715c92c2b12f0eae46a42b4413d
Juan-FlorezDm <jnendez38@gmail.com>
2024-07-20 19:24:46 -0500

    agregando Trabajo

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const btn = document.getElementById("Enviar")
+
+btn.addEventListener("click",()=>{
+    
+    const Ingresar = document.getElementById("Ingresar").value
+    const cont =  document.getElementById("respuesta")
+
+    for(var i = 1; i <= Ingresar; i++){
+        const res = document.createElement("p")
+        if(i%3===0 && i%5===0){
+            res.textContent = "fizzblitz"
+            cont.appendChild(res)
+
+        }else if(i%5===0){
+            res.textContent = "blitz"
+            cont.appendChild(res)
+            
+        }else if(i%3===0){
+            res.textContent = "fizz"
+            cont.appendChild(res)
+        }else{
+            res.textContent = i
+            cont.appendChild(res)
+            
+        }
+    }
+})
